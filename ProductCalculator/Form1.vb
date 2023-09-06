d324bb8392ec014ec8f9046b882dcb74ec9e947e
nomvula <nzshabangu95@gmail.com>
2023-09-06 11:42:22 +0200

    Add project files.

---
+++ ProductCalculator/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub btnTotal_Click(sender As Object, e As EventArgs) Handles btnTotal.Click
+        Dim Price As Decimal
+        Dim items As Integer
+        Dim Total As Decimal
+        Dim CusName As String
+
+        CusName = txtCusName.Text
+        Price = txtPrice.Text
+        items = txtQuantity.Text
+        Total = Price * items
+
+        MsgBox("hi " & CusName & " your total price is " & Total & " Rands")
+    End Sub
+End Class
